f51c7e6a522ffe80160761e984e088ab99e8434e
Dilxush-Orziqulov <orziqulovdilxush@gmail.com>
2024-01-23 15:22:52 +0500

    secondcommit

--- js/app.js
+++ js/app.js
@@ -6,8 +6,10 @@ btn.addEventListener('click', () => {
     if (dark.classList.contains('dark')) {
         dark.classList.remove('dark');
         dark.classList.add('light');
+        btn.textContent = 'Dark';
     } else {
         dark.classList.remove('light');
         dark.classList.add('dark');
+        btn.textContent = 'Light';
     }
 })
